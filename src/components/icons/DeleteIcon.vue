1c6f8a2aec1b7b771d78713feace7159a0563390
Yana Tiulenina <122269490+YaTyulen@users.noreply.github.com>
2025-04-08 01:21:40 +0700

    добавляет на отдельную ветку только файлы для развертывания

--- src/components/icons/DeleteIcon.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <svg x="0px" y="0px" viewBox="0 0 507.867 507.867" style="enable-background:new 0 0 507.867 507.867;" xml:space="preserve">
-        <g transform="matrix(0.7071 -0.7071 0.7071 0.7071 -13.255 32)">
-            <rect x="-15.994" y="34.743" style="fill:#e20000;" width="95.992" height="622.24"></rect>
-            <rect x="-279.122" y="297.871" style="fill:#e20000;" width="622.24" height="95.992"></rect>
-        </g>
-    </svg>
-</template>
